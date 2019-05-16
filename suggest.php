620e27ef9b1e2a722751fe23206fc0052e21a6d2
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-05-16 16:56:40 +0100

    Form refactor

--- suggest.php
+++ suggest.php
@@ -1,5 +1,22 @@
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $details = $_POST["details"];
+
+  echo "<pre>";
+  $email_body = "";
+  $email_body .= "Name " . $name . "\n";
+  $email_body .= "Email " . $email . "\n";
+  $email_body .= "Details " . $details . "\n";
+  echo $email_body;
+  echo "<pre>";
+
+  // To Do: Send email
+  header("location:suggest.php?status=thanks");
+}
+
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
@@ -8,27 +25,28 @@ include("inc/header.php"); ?>
 <div class="section page">
   <div class="wrapper">
     <h1>Suggest a Media Item</h1>
-    <p>Complete the form to send an email and suggest a book, movie or music that is missing.</p>
-    <form method="post" action="process.php">
-      <table>
-        <tr>
-          <th><label for="name">Name </label></th>
-          <td><input type="text" id="name" name="name"></td>
-        </tr>
-        <tr>
-          <th><label for="email">Email </label></th>
-          <td><input type="text" id="email" name="email"></td>
-        </tr>
-        <tr>
-          <th><label for="details">Suggest Item Details </label></th>
-          <td><textarea id="details" name="details"></textarea></td>
-        </tr>
-      </table>
-      <input type="submit" value="Send">
-
-
-
-    </form>
+    <?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
+      echo "<p> Thanks for the email! I'll check out your suggestion shortly!</p>";
+    } else { ?>
+      <p>Complete the form to send an email and suggest a book, movie or music that is missing.</p>
+      <form method="post" action="suggest.php">
+        <table>
+          <tr>
+            <th><label for="name">Name </label></th>
+            <td><input type="text" id="name" name="name"></td>
+          </tr>
+          <tr>
+            <th><label for="email">Email </label></th>
+            <td><input type="text" id="email" name="email"></td>
+          </tr>
+          <tr>
+            <th><label for="details">Suggest Item Details </label></th>
+            <td><textarea id="details" name="details"></textarea></td>
+          </tr>
+        </table>
+        <input type="submit" value="Send">
+      </form>
+    <?php } ?>
   </div>
 </div>
 
