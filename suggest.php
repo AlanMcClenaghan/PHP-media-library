cf18225f72cf454a24a1fbaf1f54e79a6193c94b
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-05-16 19:58:40 +0100

    SMTP with Google

--- suggest.php
+++ suggest.php
@@ -1,9 +1,16 @@
 <?php
 
+//Import the PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+
+require 'vendor/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
   $email = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
-  $details = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS));
+  $details = trim(filter_input(INPUT_POST, "details", FILTER_SANITIZE_SPECIAL_CHARS));
 
   if ($name == "" || $email == "" || $details == "") {
     echo "Please fill in the required fields: Name, Email and Details";
@@ -14,15 +21,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit;
   }
 
-  echo "<pre>";
+  if (!PHPMailer::validateAddress($email)) {
+    echo "Invalid Email Address";
+    exit;
+  }
+
   $email_body = "";
   $email_body .= "Name " . $name . "\n";
   $email_body .= "Email " . $email . "\n";
   $email_body .= "Details " . $details . "\n";
-  echo $email_body;
-  echo "<pre>";
 
   // To Do: Send email
+  $mail = new PHPMailer;
+  //Tell PHPMailer to use SMTP
+  $mail->isSMTP();
+  //Enable SMTP debugging
+  // 0 = off (for production use)
+  // 1 = client messages
+  // 2 = client and server messages
+  $mail->SMTPDebug = 2;
+  //Set the hostname of the mail server
+  $mail->Host = 'smtp.gmail.com';
+  // use
+  // $mail->Host = gethostbyname('smtp.gmail.com');
+  // if your network does not support SMTP over IPv6
+  //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+  $mail->Port = 587;
+  //Set the encryption system to use - ssl (deprecated) or tls
+  $mail->SMTPSecure = 'tls';
+  //Whether to use SMTP authentication
+  $mail->SMTPAuth = true;
+  //Username to use for SMTP authentication - use full email address for gmail
+  $mail->Username = "alandmcclenaghan@gmail.com";
+  //Password to use for SMTP authentication
+  $mail->Password = "cxymrqlvtilrvlrk";
+  //It's important not to use the submitter's address as the from address as it's forgery,
+  //which will cause your messages to fail SPF checks.
+  //Use an address in your own domain as the from address, put the submitter's address in a reply-to
+  $mail->setFrom('alandmcclenaghan@icloud.com', $name);
+  $mail->addReplyTo($email, $name);
+  $mail->addAddress('alandmcclenaghan@icloud.com', "Alan");
+  $mail->Subject = 'Libary suggestion from  ' . $name;
+  $mail->Body = $email_body;
+  if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+    exit;
+  }
+
   header("location:suggest.php?status=thanks");
 }
 
